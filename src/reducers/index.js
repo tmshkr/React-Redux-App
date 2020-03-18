10a58691b655ac7f9698fe73c6bd93b33af60b9a
Tim Shaker <tim@tmshkr.com>
2020-03-18 09:30:23 -0700

    initialState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,10 @@
-export const movieReducer = (state, action) => {
+import movieCache from "../data/movies";
+
+const initialState = {
+  savedList: [],
+  movieCache
+};
+
+export const movieReducer = (state = initialState, action) => {
   return state;
 };
