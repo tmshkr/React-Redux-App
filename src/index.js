b7715f54cf55799a697048fc1be77578061721ab
Tim Shaker <tim@tmshkr.com>
2020-03-18 09:30:13 -0700

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { movieReducer as reducer } from "./reducers";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
@@ -9,7 +9,8 @@ import thunk from "redux-thunk";
 import App from "./App";
 import "./index.scss";
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
