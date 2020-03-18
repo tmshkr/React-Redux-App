f6fb3d3c3560e85fbcbd05cb89b486d34fdf5e7b
Tim Shaker <tim@tmshkr.com>
2020-03-18 09:19:31 -0700

    add Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { createStore, applyMiddleware } from "redux";
+import { movieReducer as reducer } from "./reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import App from "./App";
 import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
